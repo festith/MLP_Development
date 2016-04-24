5db676b2ad0e58bd34901954dc9cf106195312a5
festith <a.festith@gmail.com>
2016-04-24 23:57:25 +0300

    Add Network class

---
+++ MLP_Project/MLP_Project/MLP/Neuron/Network.cs
@@ -0,0 +1,36 @@
+﻿namespace MLP_Network
+{
+    public class Network
+    {
+
+        /// <summary>
+        /// Get array of layers of network
+        /// </summary>
+        public Layer[] Layers { get; private set; }
+
+        /// <summary>
+        /// Initia;ize a new instance of the Network class.
+        /// </summary>
+        /// <param name="activationFunction"> Activation function. </param>
+        public Network(IFunction activationFunction)
+        {
+            CreateLayers(activationFunction);
+        }
+
+        /// <summary>
+        /// Compute output vector by input vector
+        /// </summary>
+        /// <param name="inputVector">Input vector (double[])</param>
+        /// <returns>Output vector (double[])</returns>
+        public float[] ComputeOutput(float[] inputVector)
+        {
+
+        }
+
+        private void CreateLayers(IFunction activationFunction)
+        {
+
+        }
+
+    }
+}
